2974075a36f09b156b567ed0893fb9670e7835b2
Renan Mascarenhas <renanmascg@gmail.com>
2025-02-24 16:09:09 -0300

    feat:Adiciona nova estrutura de banco de dados e rota inicial da aplicação com fastify.

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'ADMIN' })
-
-// const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
-
-console.log(userCanDeleteOtherUsers)
-// console.log(userCanInviteSomeoneElse)
